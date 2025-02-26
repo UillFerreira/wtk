1f5576e488a0a9d84ee416531ec4b02942156ccf
william-ferreira <william-ferreira@specchio.info>
2025-02-26 00:40:16 -0300

    Feito um componente de menu para ficar no topo do site. A principio é para ser um componente simples. Feito os callbacks para ingressar em um item no menu e também na saida de um item no menu para algum outro item

--- js/menu.js
+++ js/menu.js
@@ -1,72 +1,85 @@
-export function tMenu (param) {
-    this.classType  = "tMenu";
-    // Parameters default values
-    this.display    = "horizontal";
-    this.list       = [];
-    this._content   = document.getElementById("content");
-    // parameters received
-    if (param.display) this.display = param.display;
-    if (param.list) this.list = param.list;
-    if (param.content) this._content = param.content;
-    // Set css
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = "/wtk/css/tMenu.css";
-    document.head.appendChild(link);
+export class tMenu {
+    constructor(items) {
+        this.active = {};
+        this.items = items; // Array de itens do menu
+        this.content = content;
+        this.createNav(document.body);
+    } 
 
-    this._createMenu();
-}
-tMenu.prototype     = Object.create({});
-tMenu.constructor   = tMenu;
+    createNav (content) {
+        const nav = document.createElement('nav');
+        nav.className = 'navbar navbar-default navbar-static-top';
 
-tMenu.prototype._createMenu = function () {
-    this._ul = document.createElement("ul");
-    this._ul.setAttribute("class", this.display);
-    this._content.appendChild(this._ul);
+        // Cria o container
+        const container = document.createElement('div');
+        container.className = 'container-fluid';
+        nav.appendChild(container);
 
-    let li = this._menuList();
-}
-// Passa por todos os itens da lista e adiciona no menu
-tMenu.prototype._menuList = function () {
-    for (let i = 0; i < this.list.length; i++) {
-        this._addList(this.list[i]);
+        // Cria o cabeçalho do navbar
+        const navbarHeader = document.createElement('div');
+        navbarHeader.className = 'navbar-header';
+        container.appendChild(navbarHeader);        
+
+        // Cria o botão de toggle
+        const toggleButton = document.createElement('button');
+        toggleButton.type = 'button';
+        toggleButton.className = 'navbar-toggle collapsed';
+        toggleButton.setAttribute('data-toggle', 'collapse');
+        toggleButton.setAttribute('aria-expanded', 'false');
+        navbarHeader.appendChild(toggleButton);
+
+        // Cria o wrapper colapsável
+        const collapseWrapper = document.createElement('div');
+        collapseWrapper.className = 'collapse navbar-collapse';
+        container.appendChild(collapseWrapper);        
+
+        // Cria a lista de itens à esquerda
+        const leftNav = document.createElement('ul');
+        leftNav.className = 'nav navbar-nav';
+        collapseWrapper.appendChild(leftNav);        
+        this.#addToList(leftNav);
+        // O prepend coloca o elemento no topo em relação ao outros elementos filhos
+        content.prepend(nav);
     }
-}
-// Cria os elementos no menu
-tMenu.prototype._addList = function (i) {
-    let li  = document.createElement("li");
-    let a   = document.createElement("a");
-    a.setAttribute("href", "javascript:void(0)");
-    if (i["name"] != undefined ) {
-        a.innerText = i["name"];
-    } else {
-        a.innerText = "<Nameless>";
-    }
-    if (i["float"] != undefined && i["float"] == "right" && this.display == "horizontal") {
-        li.setAttribute("style", "float:right");
-    }
-    if (i["callback"] != undefined && typeof i["callback"] == "function") {
-        li.addEventListener("click", function () {
-            // Remove tudo da div content para colocar os novos elementos
-            let t = document.getElementById("content");
-            if (t != null) {
-                while (t.hasChildNodes()) {
-                    t.removeChild(t.lastChild);
-                }
+
+    #addToList(ul) {
+        let s = this;
+        this.items.forEach(function(i) {
+            const li = document.createElement('li');
+            if (i.active) {
+                li.classList.add('active');
+                s.active.element = li;
+                s.active.onExit = i.onExit;
             }
-            // Chama a função que vai criar os elementos na tela
-            i["callback"]();
+            // Cria um item de menu normal
+            const link = document.createElement('a');
+            link.textContent = i.text;
+            link.style.cursor = 'pointer';
+            link.style.userSelect = 'none';
+            if (i.callback != undefined) {
+                // Cria o manipulador de eventos com acesso ao item e ao callback
+                link.addEventListener("click", s.#setActive(i.callback, i.onExit, li, s));
+            }
+            li.appendChild(link);            
+            ul.appendChild(li);
         });
     }
-    li.appendChild(a);
-    this._ul.appendChild(li);
-}
-// Adiciona um item na lista e cria a entrada do elemento no menu
-tMenu.prototype.addItem = function (item) {
-    if (item != undefined && typeof item == "object") {
-        this.list.push(item);
-        this._addList(item);
+    // Função de controle do elemento ativo
+    #setActive (callback, exit, li, s) {
+        return function () {
+            // Chama um callback de saida do elemento anterior. Pode ser usado para apagar os elementos que foram criados
+            if (s.active.onExit != undefined) s.active.onExit();
+            // Arualiza as variaveis que controlam o elemento ativo no menu
+            if (li != s.active.element) {
+                li.className = 'active';
+                // Remove a classe active do elemento anterior
+                s.active.element.className = '';
+                // Atualiza o json de elemento ativo
+                s.active.element = li;
+                s.active.onExit = exit;
+            }
+            // Callback da chamada do item no menu
+            callback ();
+        }
     }
 }
-console.log("Init menu");
