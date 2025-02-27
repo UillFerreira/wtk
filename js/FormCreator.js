d708062bcc23d3f94e4513582de54dbcf9d55f44
william-ferreira <william-ferreira@specchio.info>
2025-02-26 23:38:52 -0300

    Suporte ao campos: text, textarea e date. Vou adicionar outros sob demanda

--- js/FormCreator.js
+++ js/FormCreator.js
@@ -1,19 +1,44 @@
 export class FormCreator {
-    constructor(template, content) {
-        console.log("Foi", template);
-        content.appendChild(this.#createForm(template));
-        /*
-        {
-            "title" : "Procedimento",
-            "fields" : [
-                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "text", "caption": "Nome", "placeholder": "Nome do procedimento", "onChange": cbk, "onKeyPress": cbkk},
-                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "textarea", "caption": "Descricão", "placeholder": "Nome do procedimento"},
-                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "text", "caption": "Código", "placeholder": "Nome do procedimento", "required": true, "validation": vld}
-            ]
+    #createInput (line) {
+        let input;
+        switch(line["type"]) {
+            case "textarea":
+                const r = (line["rows"] == undefined ? 3 : line["rows"]);
+                input = document.createElement("textarea");
+                input.setAttribute("rows", r);
+                input.classList.add("form-control");
+                break;
+            case "date":
+                input = document.createElement("input");
+                input.classList.add("form-control");
+                input.type = "date";
+                
+                break;
+            case "text":
+            default:
+                input = document.createElement("input");
+                input.classList.add("form-control");
+                input.type = "text";
+            break;
         }
-        */
+        // placeholder
+        const plcholder = (line["placeholder"] == undefined ? "" : line["placeholder"]);
+        input.placeholder = plcholder;
+        // required
+        if (line["required"] == true) input.setAttribute("required", "");
+        // Precisa por conta dos label
+        input.id = `input-${line["caption"]}`;
+        // tratar os callbacks dos campos
+        if (line["onChange"] != undefined && typeof line["onChange"] == "function") {
+            input.addEventListener("change", line["onChange"].bind(this));
+        }
+        if (line["onKeyPress"] != undefined && typeof line["onKeyPress"] == "function") {
+            input.addEventListener("keyup", line["onKeyPress"].bind(this));
+        }
+        if (line["pattern"] != undefined) input.pattern = line["pattern"];
+        if (line["title"] != undefined) input.title = line["title"];
+        return input;
     }
-
     #createForm(template) {
         const section = document.createElement("div");
         section.classList.add("bs-docs-section");
@@ -39,21 +64,29 @@ export class FormCreator {
             const label = document.createElement("label");
             label.setAttribute("for", `input-${caption}`);
             label.textContent = caption;
-
-            const input = document.createElement("input");
-            input.classList.add("form-control");
-            input.id = `input-${line.caption}`;
-            input.type = "text";
-            input.dataset.var = line.caption;
-            // Placeholder
-            const plcholder = (line["placeholder"] == undefined ? "" : line["placeholder"]);
-            input.placeholder = plcholder;
-            
+            console.log(this);
+            // Função para criar os inputs 
+            const input = this.#createInput(line);
+            // Append elements
             col.appendChild(label);
             col.appendChild(input);
             row.appendChild(col);
         });
         
         return section;        
+    }
+    constructor(template, content) {
+        console.log("Foi", template);
+        content.appendChild(this.#createForm(template));
+        /*
+        {
+            "title" : "Procedimento",
+            "fields" : [
+                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "text", "caption": "Nome", "placeholder": "Nome do procedimento", "onChange": cbk, "onKeyPress": cbkk},
+                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "textarea", "caption": "Descricão", "placeholder": "Nome do procedimento"},
+                {"col-xs": 4, "col-sm": 4, "col-md": 4, "col-lg": 4, "type": "text", "caption": "Código", "placeholder": "Nome do procedimento", "required": true, "validation": vld}
+            ]
+        }
+        */
     }
 }
